50c233756137f07f3a72c1b0aff8f704fc4e9dae
Aakash Chaudhry <vcr3at3@gmail.com>
2023-01-02 19:44:59 +0000

    Changes to be committed: 	modified:   IoT_ext.py

--- IoT_ext.py
+++ IoT_ext.py
@@ -1,33 +1,31 @@
-from umqttsimple import MQTTClient
-from machine import Pin
 import ubinascii
 import machine
 import micropython
-import _thread as th 
+import _thread as th
 import time
+from umqttsimple import MQTTClient
+from machine import Pin
 
+# Initialize output pins
 sw1 = Pin(15, Pin.OUT, value = 1)
 sw2 = Pin(2, Pin.OUT, value = 1)
 sw3 = Pin(4, Pin.OUT, value = 1)
 sw4 = Pin(5, Pin.OUT, value = 1)
 
-pingLed = Pin(4, Pin.OUT)
-
-# keep the connection between MQTT broker and client open for a specified time
+# Set the keepalive time (interval between pings to the MQTT broker)
 alive_time = 30
 
+# Set up MQTT connection
 SERVER = "io.adafruit.com"
 CLIENT_ID = "iot3xt"
-
-# subscribe to the following feeds
 TOPIC1 = b"icreate/feeds/wecreate.relay1"
 TOPIC2 = b"icreate/feeds/wecreate.relay2"
 TOPIC3 = b"icreate/feeds/wecreate.relay3"
 TOPIC4 = b"icreate/feeds/wecreate.relay4"
 
-# do this when received a message from the MQTT broker
+# Callback function to be executed when a message is received from the MQTT broker
 def sub_cb(topic, msg):
-    print((topic, msg))
+    # Update the output pins based on the received message
     if msg == b"ON" and topic == TOPIC1:
         sw1.value(0)
     elif msg == b"OFF" and topic == TOPIC1:
@@ -45,52 +43,53 @@ def sub_cb(topic, msg):
     elif msg == b"OFF" and topic == TOPIC4:
         sw4.value(1)
 
+# Function to continuously ping the MQTT broker
+def ping_wait(client):
+    while True:
+        # Sleep for the keepalive time
+        for i in range(alive_time):
+            machine.idle()
+        # Ping the broker
+        client.ping()
+        print("Pinging...")
 
+# Connect to the MQTT broker and set up subscriptions
 def main(server=SERVER):
-    c = MQTTClient(CLIENT_ID, server, user = 'icreate', password = '74733f4f39b54bf1b49a04f71f2769f1', keepalive = alive_time)
-    c.set_callback(sub_cb)
-    c.connect()
-    c.subscribe(TOPIC1)
-    c.subscribe(TOPIC2)
-    c.subscribe(TOPIC3)
-    c.subscribe(TOPIC4)
-    # retain the last published value by MQTT broker
-    c.publish(b"icreate/feeds/wecreate.relay1/get", b"hello")
-    c.publish(b"icreate/feeds/wecreate.relay2/get", b"hello")
-    c.publish(b"icreate/feeds/wecreate.relay3/get", b"hello")
-    c.publish(b"icreate/feeds/wecreate.relay4/get", b"hello")
+    # Create MQTT client
+    client = MQTTClient(CLIENT_ID, server, user = 'icreate', password = '74733f4f39b54bf1b49a04f71f2769f1', keepalive = alive_time)
+    # Set the callback function
+    client.set_callback(sub_cb)
+    # Connect to the broker
+    client.connect()
+    # Subscribe to topics
+    client.subscribe(TOPIC1)
+    client.subscribe(TOPIC2)
+    client.subscribe(TOPIC3)
+    client.subscribe(TOPIC4)
+    # Retain the last published value by MQTT broker
+    client.publish(b"icreate/feeds/wecreate.relay1/get", b"hello")
+    client.publish(b"icreate/feeds/wecreate.relay2/get", b"hello")
+    client.publish(b"icreate/feeds/wecreate.relay3/get", b"hello")
+    client.publish(b"icreate/feeds/wecreate.relay4/get", b"hello")
     print("Connected to %s, subscribed to %s, %s, %s and %s topics" % (server, TOPIC1, TOPIC2, TOPIC3, TOPIC4))
 
-    # function to continuously ping the MQTT broker in order to keep the connection alive
-    def ping_wait():
-        while True:
-            for i in range(alive_time):
-                time.sleep(1)
-            c.ping()
-            print("Pinging...")
-            #for i in range(4):
-                #pingLed.value(not pingLed.value())
-                #time.sleep(0.1)
-
-    # start a thread to ping the broker        
-    th.start_new_thread(ping_wait,())
+    # Start a thread to ping the broker
+    th.start_new_thread(ping_wait, (client,))
     
     try:
         while True:
-            c.wait_msg()
+            client.wait_msg()
 
     except OSError as e:
         print("Reconnecting...")
         machine.reset()
 
     finally:
-        c.disconnect()
+        client.disconnect()
 
 try:
-	main()
+    main()
 
 except OSError as e:
-	print("Host unreachable, Reconnecting...")
-	machine.reset()
-
-
+    print("Failed to connect to MQTT broker. Reconnecting...")
+    main()
